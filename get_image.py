39cf9f9f59933da45f93512740630f2ae6cfbdc8
olik07 <lobanova.olya07@yandex.ru>
2023-02-09 18:02:20 +0300

    zoom

--- get_image.py
+++ get_image.py
@@ -2,23 +2,24 @@ import sys
 import requests
 
 
-def getImage():
+def getImage(scale):
     coordinates = '37.530887,55.70311'
-    z = 17
 
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     map_params = {
         "ll": coordinates,
-        "z": z,
+        "z": scale,
         "l": "map"
     }
     response = requests.get(map_api_server, map_params)
 
     if not response:
-        print("Ошибка выполнения запроса.")
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
+        return
+        # print("Ошибка выполнения запроса.")
+        # print("Http статус:", response.status_code, "(", response.reason, ")")
+        # sys.exit(1)
 
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
+    if response:
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 from get_image import getImage
 
@@ -12,19 +13,30 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        getImage()
+        self.scale = 15
         self.initUI()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.scale -= 1
+            self.show_image()
+        elif event.key() == Qt.Key_PageDown:
+            self.scale += 1
+            self.show_image()
+
+    def show_image(self):
+        getImage(self.scale)
+        self.map_file = 'map.png'
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-
-        self.map_file = 'map.png'
-        self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+        self.show_image()
 
     def closeEvent(self, event):
         os.remove(self.map_file)
